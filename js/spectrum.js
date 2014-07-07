7c5553d295267863bf29ec850089b2c5faf9a811
Goodnightmg <agoodnight@mediag.com>
2014-07-07 16:07:58 -0400

    Spectrum Alpha

---
+++ js/spectrum.js
@@ -0,0 +1,61 @@
+ ;(function($,window,document,undefined){
+
+	var pluginName = 'spectrum',
+			defaults={
+				range:[]
+			};
+ 
+	function Plugin(element,options){
+		this.element = element
+		this.options = $.extend({},defaults,options);
+		this._defaults = defaults;
+		this._name = pluginName;
+		this.init(this.element,this.options);
+		
+		console.log($(element).get(0).tagName);
+	};
+	
+	Plugin.prototype.init = function(ele,opt){
+	
+		var rangePos = 0;
+		var mainLoop = function(){
+
+			var DOMtype = $(ele).get(0).tagName;
+			var clr = '#';
+			var prop;			
+			for(var i = 0 ; i<6 ; i++){
+				var num = Math.round(Math.random()*9);	
+				clr+=	num.toString();
+			}
+				
+			if(opt.range.length > 2){
+			
+				if(rangePos < opt.range.length-1 ){rangePos++}else{rangePos = 0};
+				clr = opt.range[rangePos]
+			}
+			
+			console.log(DOMtype === 'BODY');
+			
+			if( DOMtype === 'DIV' || DOMtype === 'BODY'){
+				$(ele).animate({'background-color':clr},400, function(){mainLoop();})
+			}else{
+				$(ele).animate({'color':clr},400, function(){mainLoop();})
+			}
+
+		};
+		
+		mainLoop();
+		
+	};
+	
+	$.fn[pluginName] = function(options){
+		return this.each(function(){
+			if(!$.data(this,'plugin_'+pluginName)){
+				$.data(this, 'plugin_'+pluginName,
+				new Plugin(this,options));
+			}
+		});
+	}
+	
+
+}(jQuery,window,document));
